25969c32a588254d18fbbb4ed0564bd17bc854dd
Artaxerxes Nazareno <artaxerxesnazare@hotmail.com>
2023-04-15 10:50:57 +0100

    telas contruidas

---
+++ README.md
@@ -0,0 +1,13 @@
+## Views
+
+### Pet Create
+![img.png](img.png)
+
+### Pet Details
+![img_1.png](img_1.png)
+
+### Pet List 
+![img_2.png](img_2.png)
+
+### Pet Update and Delete
+![img_3.png](img_3.png)
---
+++ app/src/main/java/com/example/petstore/pet_update.java
@@ -0,0 +1,14 @@
+package com.example.petstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pet_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_update);
+    }
+}
